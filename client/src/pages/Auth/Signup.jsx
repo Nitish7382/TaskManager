d20b4b192bdbccab3a314e515a58e794f4560fa4
Nitish Kumar Mohanty <nitishmohanty1@gmail.com>
2025-04-13 23:33:46 +0530

    Implement Signup component with form validation and ProfilePhotoSelector for user profile image

--- client/src/pages/Auth/Signup.jsx
+++ client/src/pages/Auth/Signup.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminInviteToken, setAdminInviteToken] = useState("");
+  const [error, setError] = useState(null);
 
-export default Signup
+  
+    //Handle SignUp form Submit
+    const handleSignup = async (e) => {
+      e.preventDefault();
+
+      
+      if (!fullName) {
+        setError("Please enter the full name");
+        return;
+      }
+
+      if (!validateEmail(email)) {
+        setError("Please enter a valid Email address");
+        return;
+      }
+  
+      if (!password) {
+        setError("Please enter a password");
+        return;
+      }
+  
+      setError("");
+  
+      //Signup API Call
+    }
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your's details below
+        </p>
+
+        <form onSubmit={handleSignup}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Signup;
