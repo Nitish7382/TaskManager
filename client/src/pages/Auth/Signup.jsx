a83e9a0298c79da699b8d9df47c958ae8343c52a
Nitish Kumar Mohanty <nitishmohanty1@gmail.com>
2025-04-16 23:30:38 +0530

    Implement code changes to enhance functionality and improve performance

--- client/src/pages/Auth/Signup.jsx
+++ client/src/pages/Auth/Signup.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import Input from "../../components/Inputs/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { UserContext } from "../../context/useContext";
+import uploadImage from "../../utils/uploadImage";
 
 const Signup = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -13,9 +17,14 @@ const Signup = () => {
   const [adminInviteToken, setAdminInviteToken] = useState("");
   const [error, setError] = useState(null);
 
+  const {updateUser} = useContext(UserContext)
+  const navigate = useNavigate();
+
   //Handle SignUp form Submit
   const handleSignup = async (e) => {
     e.preventDefault();
+
+    let profileImageUrl = '';
 
     if (!fullName) {
       setError("Please enter the full name");
@@ -35,6 +44,42 @@ const Signup = () => {
     setError("");
 
     //Signup API Call
+    try {
+
+      //upload image if present
+      if(profilePic){
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+        adminInviteToken,
+      });
+
+      const { token, role } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+
+        //Redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
   };
 
   return (
@@ -56,41 +101,41 @@ const Signup = () => {
               type="text"
             />
             <Input
-              vlaue={email}
+              value={email}
               onChange={({ target }) => setEmail(target.value)}
               label="Email address"
               placeholder="jhon@example.com"
               type="text"
             />
             <Input
-              vlaue={password}
-              onChange={({ target }) => setPassword(target.password)}
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
               label="Password"
               placeholder="Min 8 Characters"
               type="password"
             />
             <Input
-              vlaue={password}
-              onChange={({ target }) => setPassword(target.password)}
+              value={adminInviteToken}
+              onChange={({ target }) => setAdminInviteToken(target.value)}
               label="Admin Invite Token"
               placeholder="6 Digits code"
               type="text"
             />
           </div>
 
-            {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-            <button type="submit" className="btn-primary" onClick={handleSignup}>
-              SIGN UP
-            </button>
+          <button type="submit" className="btn-primary" onClick={handleSignup}>
+            SIGN UP
+          </button>
 
-            <p className="text-[13px] text-slate-800 mt-3">
-              Already have an account{" "}
-              <Link className="font-medium text-primary underline" to="/login">
-                {" "}
-                Login
-              </Link>
-            </p>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account{" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              {" "}
+              Login
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
